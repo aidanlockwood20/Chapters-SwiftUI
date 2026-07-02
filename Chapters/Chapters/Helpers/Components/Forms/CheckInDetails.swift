5347d3db63f0f44706cc40353c42b5b103e05c21
Aidan Lockwood <aidanlockwood@yahoo.com.au>
2026-07-02 17:11:19 +1000

    Update CheckInDetails.swift
    Added the option to place the check in into existing chapters

--- Chapters/Chapters/Helpers/Components/Forms/CheckInDetails.swift
+++ Chapters/Chapters/Helpers/Components/Forms/CheckInDetails.swift
@@ -1,6 +1,11 @@
 import SwiftUI
+import SwiftData
 
 struct CheckInDetails: View {
+    
+    @Query var chapters: [Chapter]
+    
+    @State var selectedChapter: Chapter?
     
     @Binding var checkInTitle: String
     @Binding var notesNavPath: [CheckInNavigation]
@@ -15,6 +20,7 @@ struct CheckInDetails: View {
                 }
                 HStack {
                     Text("Add some notes (optional)")
+                        .bold()
                     Spacer()
                     Button(action: {
                         notesNavPath.append(.noteTaking)
@@ -24,7 +30,15 @@ struct CheckInDetails: View {
                     })
                 }
                 HStack {
-                    
+                    Text("Add to an Existing Chapter")
+                        .bold()
+                    Spacer()
+                    Picker("Add to an Existing Chapter", selection: $selectedChapter) {
+                        ForEach(chapters, id: \.self) { chapter in
+                            Text(chapter.title)
+                        }
+                    }
+
                 }
             }
             .padding(20)
